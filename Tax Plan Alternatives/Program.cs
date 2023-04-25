2a7a04bb4be595ae8d3b1396207909ea6322381e
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-25 20:35:19 +0500

    properties

--- Tax Plan Alternatives/Program.cs
+++ Tax Plan Alternatives/Program.cs
@@ -8,4 +8,13 @@
     private double education;
     private double healthCare;
     private double vacations;
+
+    // properties
+    public double Housing { get => housing; set => housing = value; }
+    public double Food { get => food; set => food = value; }
+    public double Clothing { get => clothing; set => clothing = value; }
+    public double Transportation { get => transportation; set => transportation = value; }
+    public double Education { get => education; set => education = value; }
+    public double HealthCare { get => healthCare; set => healthCare = value; }
+    public double Vacations { get => vacations; set => vacations = value; }
 }
