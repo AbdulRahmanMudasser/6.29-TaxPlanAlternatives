6d510fb8b237de66c3bf8e4042ef6b80ad23c54d
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-25 20:55:21 +0500

    main methid

--- Tax Plan Alternatives/Program.cs
+++ Tax Plan Alternatives/Program.cs
@@ -17,4 +17,69 @@
     public double Education { get => education; set => education = value; }
     public double HealthCare { get => healthCare; set => healthCare = value; }
     public double Vacations { get => vacations; set => vacations = value; }
+
+    // constructor
+    public TaxPlan(double housing, double food, double clothing, double transportation, double education, double healthCare, double vacations)
+    {
+        Housing = housing;
+        Food = food;
+        Clothing = clothing;
+        Transportation = transportation;
+        Education = education;
+        HealthCare = healthCare;
+        Vacations = vacations;
+    }
+
+    // constructor
+    public TaxPlan() { }
+
+    // total expenses method
+    public double TotalExpenses()
+    {
+        double totalExpenses = Housing + Food + Clothing + Transportation + Education + HealthCare + Vacations;
+        return totalExpenses;
+    }
+
+    // fair tax
+    public double FairTax()
+    {
+        return TotalExpenses() * 0.23;
+    }
+}
+// end of TaxPlan class
+
+class TaxPlanTest
+{
+    static void Main(string[] args)
+    {
+        TaxPlan taxPlan = new TaxPlan();
+
+        // taking user input for housing expenses
+        Console.Write("Enter Housing Expenses: ");
+        taxPlan.Housing = Convert.ToDouble(Console.ReadLine());
+
+        // food
+        Console.Write("Enter Food Expenses: ");
+        taxPlan.Food = Convert.ToDouble(Console.ReadLine());
+
+        // clothing
+        Console.WriteLine("Enter Clothing Expenses: ");
+        taxPlan.Clothing = Convert.ToDouble(Console.ReadLine());
+
+        // transportation
+        Console.WriteLine("Enter Transportation Expenses: ");
+        taxPlan.Transportation = Convert.ToDouble(Console.ReadLine());
+
+        // education
+        Console.WriteLine("Enter Education Expenses: ");
+        taxPlan.Education = Convert.ToDouble(Console.ReadLine());
+
+        // healthcare
+        Console.WriteLine("Enter Health Care Expenses: ");
+        taxPlan.HealthCare = Convert.ToDouble(Console.ReadLine());
+
+        // vacations
+        Console.WriteLine("Enter Vacations Expenses: ");
+        taxPlan.Vacations = Convert.ToDouble(Console.ReadLine());
+    }
 }
